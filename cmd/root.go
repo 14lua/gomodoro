09216a6092b363063d8a58f45b969d2ca182ad10
14lua <xhigoliluan022@gmail.com>
2025-04-23 13:03:40 +0200

    add root cmd

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "gomo",
+	Short: "gomo is a cli tool to track your pomodoro sessions.",
+	Long: "gomo is a cli tool to track your pomodoro sessions. it shows you pretty timers and all ^.^",
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Oh no. An error while executing gomo: %s", err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/14lua/gomodoro/cmd"
+
+func main() {
+	cmd.Execute()
+}
